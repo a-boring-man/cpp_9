0674da418905d556727125bc224dbd31e6d3e66e
Jonathan Rinna <jonathan.rinna@gmail.com>
2023-04-26 15:49:59 +0200

    map

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: jrinna <jrinna@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 10:53:43 by jrinna            #+#    #+#             */
-/*   Updated: 2023/04/26 14:44:02 by jrinna           ###   ########lyon.fr   */
+/*   Updated: 2023/04/26 15:49:39 by jrinna           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,7 +76,7 @@ std::ostream &			operator<<( std::ostream & o, BitcoinExchange const & i )
 
 void BitcoinExchange::build_map(ifstream & file) {
 
-	string line, tmp;
+	string line, tmp1, tmp2;
 	if (file.good())
 			std::getline(file, line);
 	while (file.good())
@@ -84,13 +84,35 @@ void BitcoinExchange::build_map(ifstream & file) {
 		std::getline(file, line);
 		std::stringstream tmp_stream(line);
 		cerr << "buffer contain : ->" << line  << "<- ending here" << endl;
-		getline(tmp_stream, tmp, ',');
-		cerr << "tmp1 contain : ->" << tmp  << "<- ending here" << endl;
-		getline(tmp_stream, tmp, ',');
-		cerr << "tmp2 contain : ->" << tmp << "<- ending here" << endl;
+		getline(tmp_stream, tmp1, ',');
+		cerr << "tmp1 contain : ->" << tmp1  << "<- ending here" << endl;
+		getline(tmp_stream, tmp2, ',');
+		cerr << "tmp2 contain : ->" << tmp2 << "<- ending here" << endl;
+		rate_table.insert(std::pair<string, double>(tmp1, strtod(tmp2.c_str(), NULL)));
 		cerr << "buffer contain : ->" << line  << "<- ending here" << endl;	
 	}
+	for (map<string, double>::iterator it = rate_table.begin(); it != rate_table.end(); it++) {
+		cerr << "->" << it->first << "<- : ->" << it->second << "<-" << endl;
+	}
 }
+
+double	BitcoinExchange::get_the_value(const string & key) {
+	map<string, double>::const_iterator it = rate_table.find(key);
+	if (it != rate_table.end())
+		return it->second;
+	if (rate_table.size() == 1) {
+		return rate_table[0];
+	}
+	for (it = rate_table.begin(); it != rate_table.end(); it++) {
+		int cmp = it->first.compare(key);
+		if (cmp > 0) {
+			it--;
+			return it->second;
+		}
+	}
+	return rate_table.rbegin()->second;
+}
+
 /*
 ** --------------------------------- ACCESSOR ---------------------------------
 */
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: jrinna <jrinna@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 10:53:40 by jrinna            #+#    #+#             */
-/*   Updated: 2023/04/26 14:39:23 by jrinna           ###   ########lyon.fr   */
+/*   Updated: 2023/04/26 14:56:13 by jrinna           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <map>
 # include <fstream>
 # include <sstream>
+# include <cstdlib>
 
 using std::map;
 using std::string;
@@ -38,10 +39,13 @@ class BitcoinExchange
 
 	private:
 
+		map<string, double>	rate_table;
+		
+		double get_the_value(const string & key);
+		void build_map(ifstream & file);
+	
 		BitcoinExchange( BitcoinExchange const & src );
 		BitcoinExchange();
-		void build_map(ifstream & file);
-		map<string, float>	rate_table;
 
 };
 
